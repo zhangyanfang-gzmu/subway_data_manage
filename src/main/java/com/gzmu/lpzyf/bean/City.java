103c3e8fed3a3357a53fd4cb8dde94902dc4c839
zhangyanfang-gzmu <1424270288@qq.com>
2021-12-31 10:35:57 +0800

    张延芳：修改注解Getter Setter为get set方法

--- src/main/java/com/gzmu/lpzyf/bean/City.java
+++ src/main/java/com/gzmu/lpzyf/bean/City.java
@@ -1,14 +1,50 @@
 package com.gzmu.lpzyf.bean;
 
-import lombok.*;
 
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 public class City {
     private Integer id;
     private String nameCn;
     private String nameEn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNameCn() {
+        return nameCn;
+    }
+
+    public void setNameCn(String nameCn) {
+        this.nameCn = nameCn;
+    }
+
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    public void setNameEn(String nameEn) {
+        this.nameEn = nameEn;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", nameCn='" + nameCn + '\'' +
+                ", nameEn='" + nameEn + '\'' +
+                '}';
+    }
+
+    public City(Integer id, String nameCn, String nameEn) {
+        this.id = id;
+        this.nameCn = nameCn;
+        this.nameEn = nameEn;
+    }
+
+    public City() {
+    }
 }
